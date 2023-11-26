30bfc6a2a06b424550884c077e22d2d1564825a4
Maciek Wybraniec <rogalbiznesu@gmail.com>
2023-11-26 10:24:37 +0100

    modified:   .github/workflows/main.yml 	modified:   memgpt/autogen/examples/agent_groupchat.py 	modified:   pyproject.toml

--- memgpt/autogen/examples/agent_groupchat.py
+++ memgpt/autogen/examples/agent_groupchat.py
@@ -18,7 +18,7 @@ from memgpt.autogen.memgpt_agent import create_autogen_memgpt_agent, create_memg
 config_list = [
     {
         "model": "gpt-4",
-        "api_key": os.getenv("OPENAI_API_KEY"),
+        "api_key": os.getenv("sk-12345"),
     }
 ]
 
@@ -34,9 +34,9 @@ config_list_memgpt = [
 # # See https://github.com/oobabooga/text-generation-webui/tree/main/extensions/openai
 config_list = [
     {
-        "model": "YOUR_MODEL",  # ex. This is the model name, not the wrapper
-        "api_base": "YOUR_URL",  # ex. "http://127.0.0.1:5001/v1" if you are using webui, "http://localhost:1234/v1/" if you are using LM Studio
-        "api_key": "NULL",  # this is a placeholder
+        "model": "Dolphin-Llama-13B-GGML",  # ex. This is the model name, not the wrapper
+        "api_base": "http://localhost:1234/v1",  # ex. "http://127.0.0.1:5001/v1" if you are using webui, "http://localhost:1234/v1/" if you are using LM Studio
+        "api_key": "sk-12345",  # this is a placeholder
         "api_type": "open_ai",
     },
 ]
@@ -51,7 +51,7 @@ config_list = [
 # #   BACKEND_TYPE=lmstudio
 # # "model" here specifies the "wrapper" that will be used, setting it to "gpt-4" uses the default
 config_list_memgpt = [
-    {"model": "airoboros-l2-70b-2.1"},  # if you set this to gpt-4, it will fall back to the default wrapper
+    {"model": "gpt-4"},  # if you set this to gpt-4, it will fall back to the default wrapper
 ]
 
 
@@ -80,7 +80,7 @@ user_proxy = autogen.UserProxyAgent(
     system_message="A human admin.",
     code_execution_config={"last_n_messages": 2, "work_dir": "groupchat"},
     human_input_mode="TERMINATE",  # needed?
-    default_auto_reply="...",  # Set a default auto-reply message here (non-empty auto-reply is required for LM Studio)
+    default_auto_reply="Thank you for your message.",  # Set a default auto-reply message here (non-empty auto-reply is required for LM Studio)
 )
 
 # The agent playing the role of the product manager (PM)
@@ -88,7 +88,7 @@ pm = autogen.AssistantAgent(
     name="Product_manager",
     system_message="Creative in software product ideas.",
     llm_config=llm_config,
-    default_auto_reply="...",  # Set a default auto-reply message here (non-empty auto-reply is required for LM Studio)
+    default_auto_reply="Thank you for your message.",  # Set a default auto-reply message here (non-empty auto-reply is required for LM Studio)
 )
 
 if not USE_MEMGPT:
@@ -123,11 +123,11 @@ else:
         )
 
 # Initialize the group chat between the user and two LLM agents (PM and coder)
-groupchat = autogen.GroupChat(agents=[user_proxy, pm, coder], messages=[], max_round=12)
+groupchat = autogen.GroupChat(agents=[user_proxy, pm, coder], messages=[], max_round=50)
 manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
 
 # Begin the group chat with a message from the user
 user_proxy.initiate_chat(
     manager,
-    message="I want to design an app to make me one million dollars in one month. " "Yes, your heard that right.",
+    message="Write me numbers from 1 to 15",
 )
